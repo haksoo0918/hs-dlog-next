6bf563f2455636e1cd9c83f55fb08b42d68791be
haksoo <haksoo0918@gmail.com>
2025-01-13 15:47:19 +0900

    🔧 기본 설정 변경

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,16 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: 'Open mind',
+    description: `익명 질문 및 답변 서비스`,
     imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    href: 'https://team8-openmind.netlify.app/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
+    title: 'Wikied',
+    description: `남들이 만드는 나만의 위키 서비스`,
     imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    href: 'https://wikied-ten.vercel.app/',
   },
 ]
 
