3b7cbb3c4114a7a0f34a89d31b0264edec48a9ef
haksoo <haksoo0918@gmail.com>
2025-01-14 20:03:26 +0900

    📝 블로그 글 등록

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: '화면 개발 관련 또는 잡담 - 기록',
   language: 'ko',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://haksoo0918.github.io/hs-dlog-next/',
+  siteRepo: 'https://github.com/haksoo0918/hs-dlog-next',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   // mastodon: 'https://mastodon.social/@mastodonuser',
