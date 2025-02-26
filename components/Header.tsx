182d33672702bd3c69d4767c7ab8c60affaf452a
haksoo <haksoo0918@gmail.com>
2025-02-26 20:59:11 +0900

    💄 gnb 애니메이션 스타일 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,6 +13,7 @@ const Header = () => {
   return (
     <header
       className={cn(
+        'gnb',
         siteMetadata.stickyNav && 'sticky top-0 z-50',
         'flex w-full items-center justify-between bg-white py-10 dark:bg-gray-950'
       )}
@@ -24,7 +25,13 @@ const Header = () => {
       >
         <div className="flex items-center justify-between">
           <div className="mr-3">
-            <Image src="/static/favicons/logo.svg" alt="HS Logo" width={64} height={64} />
+            <Image
+              src="/static/favicons/logo.svg"
+              alt="HS Logo"
+              className="logo"
+              width={64}
+              height={64}
+            />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold sm:block">
